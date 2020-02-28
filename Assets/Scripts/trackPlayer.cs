3087be6cfae8960d1e66984349be32fff5422824
Versawe <47580432+Versawe@users.noreply.github.com>
2020-02-28 00:08:35 -0500

    2/28 - late night
    started the spawning of multiple blocks when you go in one direction. this will make a never ending block to drive down or never ending expressway will be put to use. Still need to test a little bit more but i am tired rn

---
+++ Assets/Scripts/trackPlayer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trackPlayer : MonoBehaviour
+{
+    GameObject player;
+
+    public Transform thisObject;
+
+    public GameObject nextObject;
+
+    public Transform spawnPoint;
+
+    public Transform spawnPointBack;
+
+    private float spawnLimit = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.Find("carBox"))
+        {
+            player = GameObject.Find("carBox");
+        }
+    }
+
+    // Update is called once per frame
+    // Need to start player near the middle of the block to spawn on back side
+    void Update()
+    {
+        if (player.transform.position.z > thisObject.position.z - 20 && player.transform.position.z < thisObject.position.z + 20)
+        {
+            spawnLimit += 1;
+        }
+        else
+        {
+            spawnLimit = 0;
+        }
+
+        if (spawnLimit > 0 && spawnLimit < 2)
+        {
+            Instantiate(nextObject, spawnPoint.position, spawnPoint.rotation);
+            Instantiate(nextObject, spawnPointBack.position, spawnPointBack.rotation);
+            print("spawned");
+        }
+        if (player.transform.position.z > thisObject.position.z + 350)
+        {
+            Destroy(gameObject);
+            print("boom");
+        }
+        if (player.transform.position.z < thisObject.position.z - 350)
+        {
+            Destroy(gameObject);
+            print("boom");
+        }
+
+        //print(spawnLimit);
+    }
+}
