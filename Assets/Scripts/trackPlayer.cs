694d0dcf4b28e8a7453ced628c1e5dd1405e3af0
Versawe <47580432+Versawe@users.noreply.github.com>
2020-02-29 13:39:11 -0500

    2/29
    Made it so you can't go backwards which limits the user's direction which will be best for exercises. this way doesn't spawn too many blocks at onces which is good because I plan to have mutliple different blocks spawn randomly.

--- Assets/Scripts/trackPlayer.cs
+++ Assets/Scripts/trackPlayer.cs
@@ -12,47 +12,49 @@ public class trackPlayer : MonoBehaviour
 
     public Transform spawnPoint;
 
-    public Transform spawnPointBack;
+    public Transform triggerSpawnPoint;
+    public Transform triggerBoxCollider;
 
     private float spawnLimit = 0;
+
+    BoxCollider bc;
 
     // Start is called before the first frame update
     void Start()
     {
+        bc = GetComponent<BoxCollider>();
+        bc.enabled = false;
+
         if (GameObject.Find("carBox"))
         {
             player = GameObject.Find("carBox");
         }
+
     }
 
     // Update is called once per frame
     // Need to start player near the middle of the block to spawn on back side
     void Update()
     {
-        if (player.transform.position.z > thisObject.position.z - 20 && player.transform.position.z < thisObject.position.z + 20)
+        if (player.transform.position.z > triggerSpawnPoint.position.z)
         {
             spawnLimit += 1;
-        }
-        else
-        {
-            spawnLimit = 0;
         }
 
         if (spawnLimit > 0 && spawnLimit < 2)
         {
             Instantiate(nextObject, spawnPoint.position, spawnPoint.rotation);
-            Instantiate(nextObject, spawnPointBack.position, spawnPointBack.rotation);
             print("spawned");
         }
-        if (player.transform.position.z > thisObject.position.z + 350)
+        if (player.transform.position.z > thisObject.position.z + 300)
         {
             Destroy(gameObject);
             print("boom");
         }
-        if (player.transform.position.z < thisObject.position.z - 350)
+
+        if (player.transform.position.z > triggerBoxCollider.position.z)
         {
-            Destroy(gameObject);
-            print("boom");
+            bc.enabled = true;
         }
 
         //print(spawnLimit);
