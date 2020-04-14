dffc5e19c444ee4755439e8cce733d3a4721dbac
Eric Versaw <47580432+Versawe@users.noreply.github.com>
2020-04-14 03:06:36 -0400

    4/14 - Main Menu
    Made a functional greybox of a main menu. Also found a glitch with super cruise and tried to fix it best I could.

---
+++ Assets/loadLevelScene.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadLevelScene : MonoBehaviour
+{
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Touch touch in Input.touches) 
+         {
+            if(touch.position.x < Screen.width) 
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if(hit.collider.name == "button1") 
+                    {
+                        SceneManager.LoadScene(1);
+                    }
+                    if (hit.collider.name == "button2")
+                    {
+                        SceneManager.LoadScene(2);
+                    }
+                    if (hit.collider.name == "button3")
+                    {
+                        SceneManager.LoadScene(3);
+                    }
+
+                }
+            }
+            
+        }
+
+    }
+
+}
