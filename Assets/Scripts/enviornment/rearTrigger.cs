d3d9f976c5db5a5fd2a653e8639e3974349bc95b
RSSpeacemaker <47580403+RSSpeacemaker@users.noreply.github.com>
2020-04-23 06:37:06 -0400

    4/23
    Cross Traffic Alert is blinking properly.

--- Assets/Scripts/enviornment/rearTrigger.cs
+++ Assets/Scripts/enviornment/rearTrigger.cs
@@ -9,18 +9,29 @@ public class rearTrigger : MonoBehaviour
     private bool isAlerting = false;
     private bool leftAlert = false;
     private bool rightAlert = false;
+    private int blinkDelayRight = 0;
+    private int blinkCountRight = 0;
+    private int blinkDelayLeft = 0;
+    private int blinkCountLeft = 0;
+    private int switchOnAndOffRight = 0;
+    private int switchOnAndOffLeft = 0;
+    private bool uiCrossTrafficRightActive = false;
+    private bool uiCrossTrafficleftActive = false;
+    public GameObject uiCrossTrafficRight;
+    public GameObject uiCrossTrafficLeft;
     public GameObject playerCar;
     GameObject nearPlayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiCrossTrafficRight.SetActive(false);
+        uiCrossTrafficLeft.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isAutoBraking) 
+        if (isAutoBraking)
         {
             if (gearShifterScript.inReverse)
             {
@@ -49,19 +60,69 @@ public class rearTrigger : MonoBehaviour
         }
         if (rightAlert) 
         {
-            print("Right ALERTTTTT");
+            //print("Right ALERTTTTT");
             if (!textTriggers.activateSecondText && safteyFeature.crossTrafficTrigger) 
             {
                 textTriggers.activateSecondText = true;
             }
+
+            if(blinkCountRight < 9 && blinkDelayRight <= 0)
+            {
+                switch (switchOnAndOffRight)
+                {
+                    case 0:
+                        switchOnAndOffRight = 1;
+                        uiCrossTrafficRight.SetActive(true);
+                        break;
+                    case 1:
+                        switchOnAndOffRight = 0;
+                        uiCrossTrafficRight.SetActive(false);
+                        break;
+                }
+                blinkDelayRight = 20;
+                blinkCountRight += 1;
+            }
+            blinkDelayRight -= 1;
         }
         if (leftAlert) 
         {
-            print("Left Alert");
+            //print("Left Alert");
             if (!textTriggers.activateSecondText && safteyFeature.crossTrafficTrigger)
             {
                 textTriggers.activateSecondText = true;
             }
+
+            if (blinkCountLeft < 9 && blinkDelayLeft <= 0)
+            {
+                switch (switchOnAndOffLeft)
+                {
+                    case 0:
+                        switchOnAndOffLeft = 1;
+                        uiCrossTrafficLeft.SetActive(true);
+                        break;
+                    case 1:
+                        switchOnAndOffLeft = 0;
+                        uiCrossTrafficLeft.SetActive(false);
+                        break;
+                }
+                blinkDelayLeft = 20;
+                blinkCountLeft += 1;
+            }
+            blinkDelayLeft -= 1;
+        }
+        if(!rightAlert)
+        {
+            blinkDelayRight = 0;
+            blinkCountRight = 0;
+            uiCrossTrafficRight.SetActive(false);
+            switchOnAndOffRight = 0;
+        }
+        if (!leftAlert)
+        {
+            blinkDelayLeft = 0;
+            blinkCountLeft = 0;
+            uiCrossTrafficLeft.SetActive(false);
+            switchOnAndOffLeft = 0;
         }
 
         //print(safteyFeature.autoRearBrakeTrigger);
